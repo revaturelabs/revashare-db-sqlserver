dc0eb05698c5f2f9b6b71952ca78f7497238aa0e
greg mckinney <gmck95@msn.com>
2016-12-14 13:54:33 -0500

    Ride and Rider mappers and wiring up of a few methods in the service [#136046045]

--- RevaShare/RevaShare.DataClient/IRevaShareDataService.cs
+++ RevaShare/RevaShare.DataClient/IRevaShareDataService.cs
@@ -8,17 +8,36 @@ using System.Text;
 
 namespace RevaShare.DataClient
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRevaShareDataService" in both code and config file together.
-    [ServiceContract]
-    public interface IRevaShareDataService
-    {
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRevaShareDataService" in both code and config file together.
+  [ServiceContract]
+  public interface IRevaShareDataService
+  {
 
-        [OperationContract]
-        RideDAO passRide();
-        [OperationContract]
-        UserDAO passUser();
-        [OperationContract]
-        RideRidersDAO passRideRider();
+    [OperationContract]
+    RideDAO passRide();
+    [OperationContract]
+    UserDAO passUser();
+    [OperationContract]
+    RideRidersDAO passRideRider();
+    [OperationContract]
+    UserDAO logIn();
+    [OperationContract]
+    UserDAO register();
 
-    }
+    [OperationContract]
+    ApartmentDAO GetApartment(int id);
+    [OperationContract]
+    ApartmentDAO GetApartmentByName(string name);
+
+    [OperationContract]
+    RideRidersDAO GetRideRiders();
+    [OperationContract]
+    RideRidersDAO GetRideRiderById(int id);
+
+    [OperationContract]
+    VehicleDAO GetVehicleById(int id);
+
+
+
+  }
 }
--- RevaShare/RevaShare.DataClient/RevaShareDataService.svc.cs
+++ RevaShare/RevaShare.DataClient/RevaShareDataService.svc.cs
@@ -1,4 +1,5 @@
-﻿using RevaShare.DataClient.Models;
+﻿using RevaShare.DataAccess.Data;
+using RevaShare.DataClient.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +9,59 @@ using System.Text;
 
 namespace RevaShare.DataClient
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RevaShareDataService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select RevaShareDataService.svc or RevaShareDataService.svc.cs at the Solution Explorer and start debugging.
-    public class RevaShareDataService : IRevaShareDataService
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RevaShareDataService" in code, svc and config file together.
+  // NOTE: In order to launch WCF Test Client for testing this service, please select RevaShareDataService.svc or RevaShareDataService.svc.cs at the Solution Explorer and start debugging.
+  public class RevaShareDataService : IRevaShareDataService
+  {
+    RevaShareData data = new RevaShareData();
+    public ApartmentDAO GetApartment(int id)
     {
-
-        public RideDAO passRide()
-        {
-            return new RideDAO();
-        }
-
-        public RideRidersDAO passRideRider()
-        {
-            return new RideRidersDAO();
-        }
-
-        public UserDAO passUser()
-        {
-            return new UserDAO();
-        }
+      return ApartmentMapper.MapToApartmentDAO(data.GetApartment(id));
     }
+
+    public RideRidersDAO GetRideRiderById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public RideRidersDAO GetRideRiders()
+    {
+      throw new NotImplementedException();
+    }
+
+    public VehicleDAO GetVehicleById(int id)
+    {
+      return VehicleMapper.MapToVehicleDAO(data.GetVehicleByID(id));
+    }
+
+    public ApartmentDAO GetApartmentByName(string name)
+    {
+      return ApartmentMapper.MapToApartmentDAO(data.GetApartmentByName(name));
+    }
+
+    public UserDAO logIn()
+    {
+      throw new NotImplementedException();
+    }
+
+    public RideDAO passRide()
+    {
+      return new RideDAO();
+    }
+
+    public RideRidersDAO passRideRider()
+    {
+      return new RideRidersDAO();
+    }
+
+    public UserDAO passUser()
+    {
+      return new UserDAO();
+    }
+
+    public UserDAO register()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
