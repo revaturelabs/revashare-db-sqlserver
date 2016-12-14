456bdf5325b8350cf84a3b6e5093297ee02d1789
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-14 15:25:45 -0500

    Add RoleDAO and add RoleDAOs to UserDAO.

---
+++ RevaShare/RevaShare.DataClient/Models/RoleDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RevaShare.DataClient.Models {
+    [DataContract]
+    public class RoleDAO {
+        [DataMember]
+        public string Type { get; set; }
+    }
+}
