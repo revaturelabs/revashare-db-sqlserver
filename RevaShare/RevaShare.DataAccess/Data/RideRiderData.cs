870756fb76a9b9754dfa7d622690301fcb44d790
greg mckinney <gmck95@msn.com>
2016-12-14 10:32:16 -0500

    Vehicle Crud and Basic RideRider Crud [#136046045]

--- RevaShare/RevaShare.DataAccess/Data/RideRiderData.cs
+++ RevaShare/RevaShare.DataAccess/Data/RideRiderData.cs
@@ -4,7 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RevaShare.DataAccess.Data {
-    public partial class RevaShareData {
+namespace RevaShare.DataAccess.Data
+{
+  public partial class RevaShareData
+  {
+    public List<RideRider> GetRideRiders()
+    {
+      
+      return context.RideRiders.ToList();
     }
+
+    public bool AddRideRider(Ride ride, RideRider riderider)
+    {     
+      riderider.RideID = ride.ID;
+      //v.ID = r.VehicleID;
+      
+      context.RideRiders.Add(riderider);
+      return context.SaveChanges() > 0;
+    }
+
+    public bool UpdateRideRider(RideRider riderider)
+    {
+      var result = context.RideRiders.SingleOrDefault(x => x.RiderID == riderider.RiderID);
+
+      if (result != null)
+      {
+        if (riderider.RideID != 0)
+          result.RideID = riderider.RideID;
+
+        if (riderider.RiderID != null)
+          result.RiderID = riderider.RiderID;
+
+        result.Accepted = riderider.Accepted;        
+
+        result.Active = riderider.Active;
+      }
+      return context.SaveChanges() > 0;
+    }
+
+    public bool DeleteRideRider(string id)
+    {
+      var riderider = context.RideRiders.Where(x => x.RiderID == id).FirstOrDefault();
+      context.RideRiders.Remove(riderider);
+      return context.SaveChanges() > 0;
+    }
+
+    public List<RideRider> GetRideRiderByID(string id)
+    {
+      var result = context.RideRiders.Where(a => a.RiderID == id);
+      return result.ToList();
+    }
+  }
 }
--- RevaShare/RevaShare.DataAccess/Data/VehicleData.cs
+++ RevaShare/RevaShare.DataAccess/Data/VehicleData.cs
@@ -4,7 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RevaShare.DataAccess.Data {
-    public partial class RevaShareData {
+namespace RevaShare.DataAccess.Data
+{
+  public partial class RevaShareData
+  {
+    public List<Vehicle> GetVehicles()
+    {
+      return context.Vehicles.ToList();
     }
+
+    public bool AddVehicle(Vehicle vehicle)
+    {
+      context.Vehicles.Add(vehicle);
+      return context.SaveChanges() > 0;
+    }   
+
+    public bool UpdateVehicle(Vehicle vehicle)
+    {
+      var result = context.Vehicles.SingleOrDefault(x => x.ID == vehicle.ID);
+
+      if (result != null)
+      {
+        if (vehicle.ID != 0)
+          result.ID = vehicle.ID;
+
+        if (vehicle.Make != null)
+          result.Make = vehicle.Make;
+
+        if (vehicle.Model != null)
+          result.Model = vehicle.Model;
+
+        if (vehicle.LicensePlate != null)
+          result.LicensePlate = vehicle.LicensePlate;
+
+        if (vehicle.Color != null)
+          result.Color = vehicle.Color;
+
+        if (vehicle.Capacity != 0)
+          result.Capacity = vehicle.Capacity;
+
+        if (vehicle.OwnerID != null)
+          result.OwnerID = vehicle.OwnerID;
+
+        result.Active = vehicle.Active;
+      }
+      return context.SaveChanges() > 0;
+    }
+    
+    public bool DeleteVehicle(int id)
+    {
+      var vehicle = context.Vehicles.Where(x => x.ID == id).FirstOrDefault();
+      context.Vehicles.Remove(vehicle);
+      return context.SaveChanges() > 0;
+    }
+
+    public List<Vehicle> GetVehicleByID(int id)
+    {
+      var result = context.Vehicles.Where(a => a.ID == id);
+      return result.ToList();
+    }
+  }
 }
