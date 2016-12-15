770897d1c430ca0c67c16b994e72c3d4d1b6dd14
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-15 16:07:28 -0500

    Fix apartment data service.

--- RevaShare/RevaShare.DataClient/Services/ApartmentServices.cs
+++ RevaShare/RevaShare.DataClient/Services/ApartmentServices.cs
@@ -4,39 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace RevaShare.DataClient
-{
-    public partial class RevaShareDataService
-    {
-        public ApartmentDAO GetApartmentByName(string name)
-        {
+namespace RevaShare.DataClient {
+    public partial class RevaShareDataService {
+        public ApartmentDAO GetApartmentByName(string name) {
             return ApartmentMapper.MapToApartmentDAO(data.GetApartmentByName(name));
         }
 
-        public bool AddApartment(ApartmentDAO apartment)
-        {
+        public bool AddApartment(ApartmentDAO apartment) {
             return data.CreateApartment(ApartmentMapper.MapToApartment(apartment));
         }
 
-        public List<ApartmentDAO> ListApartments()
-        {
+        public List<ApartmentDAO> ListApartments() {
             var r = new List<ApartmentDAO>();
 
-            foreach (var apartment in data.ListApartments())
-            {
+            foreach (var apartment in data.ListApartments()) {
                 r.Add(ApartmentMapper.MapToApartmentDAO(apartment));
             }
             return r;
         }
 
-        public bool UpdateApartment(ApartmentDAO apartment)
-        {
+        public bool UpdateApartment(ApartmentDAO apartment) {
             return data.UpdateApartment(ApartmentMapper.MapToApartment(apartment));
         }
 
-        public bool DeleteApartment(ApartmentDAO apartment)
-        {
-            return data.DeleteApartment(ApartmentMapper.MapToApartment(apartment));
+        public bool DeleteApartment(string apartmentName) {
+            return data.DeleteApartment(apartmentName);
         }
     }
 }
