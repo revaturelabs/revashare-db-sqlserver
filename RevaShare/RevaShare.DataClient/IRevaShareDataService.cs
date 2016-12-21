5f35eb6b11159a283fc0be3ecdfe8eeab2763a8c
jason goodell <jason.goodell@outlook.com>
2016-12-21 18:04:16 -0500

    fix reference

--- RevaShare/RevaShare.DataClient/IRevaShareDataService.cs
+++ RevaShare/RevaShare.DataClient/IRevaShareDataService.cs
@@ -8,71 +8,71 @@ using System.Text;
 
 namespace RevaShare.DataClient
 {
-  [ServiceContract]
-  public interface IRevaShareDataService
-  {
-    //User CRUD
-    [OperationContract]
-    bool RegisterUser(UserDAO user, string username, string password);
+   [ServiceContract]
+   public interface IRevaShareDataService
+   {
+      //User CRUD
+      [OperationContract]
+      bool RegisterUser(UserDAO user, string username, string password);
 
-    [OperationContract]
-    List<UserDAO> GetAllUsers();
+      [OperationContract]
+      List<UserDAO> GetAllUsers();
 
-    [OperationContract]
-    UserDAO GetUserByUsername(string username);
+      [OperationContract]
+      UserDAO GetUserByUsername(string username);
 
-    [OperationContract]
-    List<UserDAO> GetAdmins();
+      [OperationContract]
+      List<UserDAO> GetAdmins();
 
-    [OperationContract]
-    UserDAO GetAdminByUsername(string username);
+      [OperationContract]
+      UserDAO GetAdminByUsername(string username);
 
-    [OperationContract]
-    bool UpdateUser(UserDAO user);
+      [OperationContract]
+      bool UpdateUser(UserDAO user);
 
-    [OperationContract]
-    bool DeleteUser(string username);
+      [OperationContract]
+      bool DeleteUser(string username);
 
-    [OperationContract]
-    bool AddAdmin(UserDAO user, string username, string password);
+      [OperationContract]
+      bool AddAdmin(UserDAO user, string username, string password);
 
-    //Other User Related Methods
-    [OperationContract]
-    UserDAO Login(string username, string password);
+      //Other User Related Methods
+      [OperationContract]
+      UserDAO Login(string username, string password);
 
-    [OperationContract]
-    bool ApproveDriver(string username);
+      [OperationContract]
+      bool ApproveDriver(string username);
 
-    [OperationContract]
-    bool ApproveUser(string username);
+      [OperationContract]
+      bool ApproveUser(string username);
 
-    [OperationContract]
-    bool RequestToBeDriver(string username);
+      [OperationContract]
+      bool RequestToBeDriver(string username);
 
-    [OperationContract]
-    List<UserDAO> PendingRegistrations();
+      [OperationContract]
+      List<UserDAO> PendingRegistrations();
 
-    [OperationContract]
-    List<UserDAO> PendingDriverApprovals();
+      [OperationContract]
+      List<UserDAO> PendingDriverApprovals();
 
-    [OperationContract]
-    bool UpdatePassword(string username, string currentPassword, string newPassword);
+      [OperationContract]
+      bool UpdatePassword(string username, string currentPassword, string newPassword);
 
-    //Apartment section
-    [OperationContract]
-    ApartmentDAO GetApartmentByName(string name);
+      //Apartment section
+      [OperationContract]
+      ApartmentDAO GetApartmentByName(string name);
 
-    [OperationContract]
-    bool AddApartment(ApartmentDAO apartment);
+      [OperationContract]
+      bool AddApartment(ApartmentDAO apartment);
 
-    [OperationContract]
-    List<ApartmentDAO> ListApartments();
+      [OperationContract]
+      List<ApartmentDAO> ListApartments();
 
-    [OperationContract]
-    bool UpdateApartment(ApartmentDAO apartment);
+      [OperationContract]
+      bool UpdateApartment(ApartmentDAO apartment);
 
-    [OperationContract]
-    bool DeleteApartment(string apartment);
+      [OperationContract]
+      bool DeleteApartment(string apartment);
 
 
       //Ride section
@@ -97,53 +97,53 @@ namespace RevaShare.DataClient
       [OperationContract]
       List<RideDAO> ListRidesAtApartmentPM(string apartmentName);
 
-    //RideRider section
-    [OperationContract]
-    List<RideRidersDAO> GetRideRiders();
+      //RideRider section
+      [OperationContract]
+      List<RideRidersDAO> GetRideRiders();
 
-    [OperationContract]
-    bool AddRideRiders(UserDAO user, RideDAO ride);
+      [OperationContract]
+      bool AddRideRiders(UserDAO user, RideDAO ride);
 
-    [OperationContract]
-    bool UpdateRideRider(RideRidersDAO riderider);
+      [OperationContract]
+      bool UpdateRideRider(RideRidersDAO riderider);
 
-    [OperationContract]
-    bool Accept(RideRidersDAO riderider);
+      [OperationContract]
+      bool AcceptRideRequest(RideRidersDAO riderider);
 
-    [OperationContract]
-    bool DeleteRideRider(RideRidersDAO riderider);
+      [OperationContract]
+      bool DeleteRideRider(RideRidersDAO riderider);
 
-    [OperationContract]
-    List<UserDAO> getRidersInRide(RideDAO ride);
+      [OperationContract]
+      List<UserDAO> getRidersInRide(RideDAO ride);
 
-    //Vehicle section
-    [OperationContract]
-    bool AddVehicle(VehicleDAO vehicle);
+      //Vehicle section
+      [OperationContract]
+      bool AddVehicle(VehicleDAO vehicle);
 
-    [OperationContract]
-    bool UpdateVehicle(VehicleDAO vehicle);
+      [OperationContract]
+      bool UpdateVehicle(VehicleDAO vehicle);
 
-    [OperationContract]
-    bool DeleteVehicle(VehicleDAO vehicle);
+      [OperationContract]
+      bool DeleteVehicle(VehicleDAO vehicle);
 
-    [OperationContract]
-    List<VehicleDAO> GetVehicles();
+      [OperationContract]
+      List<VehicleDAO> GetVehicles();
 
-    //Flag section
-    [OperationContract]
-    bool CreateFlag(FlagDAO flag);
+      //Flag section
+      [OperationContract]
+      bool CreateFlag(FlagDAO flag);
 
-    [OperationContract]
-    FlagDAO GetFlagByID(int id);
+      [OperationContract]
+      FlagDAO GetFlagByID(int id);
 
-    [OperationContract]
-    List<FlagDAO> GetAllFlags();
+      [OperationContract]
+      List<FlagDAO> GetAllFlags();
 
-    [OperationContract]
-    bool MarkFlagAsRead(FlagDAO flag);
+      [OperationContract]
+      bool MarkFlagAsRead(FlagDAO flag);
 
-    [OperationContract]
-    bool UpdateFlag(FlagDAO flag);
+      [OperationContract]
+      bool UpdateFlag(FlagDAO flag);
 
-  }
+   }
 }
